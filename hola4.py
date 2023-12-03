4f87042bc1179758eca93e52a4c32d25d40220b2
Nelson Castellano <exncasc@bci.cl>
2023-12-03 19:02:21 -0300

    Agrego archivo 4

---
+++ hola4.py
@@ -0,0 +1,8 @@
+saludos = "Hola Mariana Castellano, Gabriel Castellano, Nelson Castellano y Carlina Martinez"
+
+print(saludos)
+
+#cambios en rama solo main*
+#otro cambio en rama main y test*
+#se agrega esta linea con rama test_nelson*
+#un cambio mas*marianateammmmmomgavriel mariana nelson carlina gabriel chiki abril
